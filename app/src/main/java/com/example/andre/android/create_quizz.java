420796ba0d6c5a120d18c51d666713d5951fb358
andreeataulea <45334277+andreeataulea@users.noreply.github.com>
2018-12-08 21:32:03 +0200

    Versiunea noua

--- app/src/main/java/com/example/andre/android/create_quizz.java
+++ app/src/main/java/com/example/andre/android/create_quizz.java
@@ -2,6 +2,8 @@ package com.example.andre.android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class create_quizz extends Activity {
 
@@ -9,5 +11,11 @@ public class create_quizz extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_quizz);
+
+        Spinner spinnerMaterii = (Spinner)findViewById(R.id.spinnerMaterie);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.materii,android.R.layout.simple_expandable_list_item_1);
+
+        spinnerMaterii.setAdapter(adapter);
     }
 }
