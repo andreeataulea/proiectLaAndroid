c206a331e7f1c88381407c628dc0205970671139
andreeataulea <45334277+andreeataulea@users.noreply.github.com>
2018-12-10 01:53:14 +0200

    Versiunea noua

--- app/src/main/java/com/example/andre/android/adapters/ReportAdapter.java
+++ app/src/main/java/com/example/andre/android/adapters/ReportAdapter.java
@@ -27,21 +27,25 @@ public class ReportAdapter extends ArrayAdapter<RaportQuizz> {
     @NonNull
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+
         String nume=getItem(position).getNumeTestR();
         String cod=getItem(position).getCodTestR();
         String punctaj=getItem(position).getPunctaj();
+        String materie = getItem(position).getMaterie();
 
-        RaportQuizz q1=new RaportQuizz(nume, cod, punctaj);
+        RaportQuizz q1=new RaportQuizz(  nume,cod, punctaj,materie);
         LayoutInflater inflater=LayoutInflater.from(mContext);
         convertView=inflater.inflate(mResource,parent,false);
 
         TextView tvtN=(TextView)convertView.findViewById(R.id.tvNumeRaport);
         TextView tvtC=(TextView)convertView.findViewById(R.id.tvCodTextRaport);
         TextView tvtP=(TextView)convertView.findViewById(R.id.tvPunctaj);
+        TextView tvM = (TextView)convertView.findViewById(R.id.tvMaterie);
 
         tvtN.setText(nume);
         tvtC.setText(cod);
         tvtP.setText(punctaj);
+        tvM.setText(materie);
 
         return convertView;
     }
--- app/src/main/java/com/example/andre/android/model/RaportQuizz.java
+++ app/src/main/java/com/example/andre/android/model/RaportQuizz.java
@@ -6,22 +6,29 @@ public class RaportQuizz implements Serializable {
    private String numeTestR;
    private String codTestR;
    private String punctaj;
+   private String materie;
 
-   public RaportQuizz (String numeTestR, String nucodTestRmeS, String punctaj){
+   public RaportQuizz ( String numeTestR,String codTestR, String punctaj, String materie){
        this.numeTestR=numeTestR;
        this.codTestR=codTestR;
        this.punctaj=punctaj;
+       this.materie = materie;
    }
 
    public String  getNumeTestR(){return numeTestR;}
    public String getCodTestR(){return codTestR;}
    public String getPunctaj(){return punctaj;}
+   public String getMaterie(){return materie;}
 
    public void setNumeTestR(String numeTestR){this.numeTestR=numeTestR;}
    public void setCodTestR(String codTestR){this.codTestR=codTestR;}
    public void setPunctaj(String punctaj){this.punctaj=punctaj;}
 
-   public String toString(){
+    public void setMaterie(String materie) {
+        this.materie = materie;
+    }
+
+    public String toString(){
        StringBuilder sb=new StringBuilder();
        sb.append(numeTestR);
        return sb.toString();
