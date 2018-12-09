bc0ebd5050f78787c7177f584747a236b49ddde1
andreeastaicu3 <staicuandreea3@gmail.com>
2018-12-09 22:38:09 +0200

    doamne ajuta

--- app/src/main/java/com/example/andre/android/FunFacts.java
+++ app/src/main/java/com/example/andre/android/FunFacts.java
@@ -1,30 +1,25 @@
 package com.example.andre.android;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.support.v7.widget.CardView;
 
 public class FunFacts extends AppCompatActivity {
-    Button click;
     public static TextView data;
+
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_funfacts);
 
-        click=(Button) findViewById(R.id.funFact);
         data=(TextView)findViewById(R.id.fetcheddata);
-
-        click.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                fetchData process=new fetchData();
-                process.execute();
-
-            }
-        });
+        fetchData process=new fetchData();
+        process.execute();
     }
 
 }
